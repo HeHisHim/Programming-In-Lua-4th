79439532d45453670059298ed2b74cadc4fc3e85
yoyboy <534411590@qq.com>
2019-03-21 00:31:09 +0800

    Create test2-1.lua

---
+++ chapter02/test2-1.lua
@@ -0,0 +1,47 @@
+-- 八皇后问题 输出第一个解后停止运行
+-- 在第一次调用printsolution之后调用os.exit()
+
+N = 8  -- 棋盘大小
+total = 0
+-- 检查(n, c)是否会被攻击
+function isplaceok(a, n, c)
+    for i = 1, n - 1 do -- 对于每一个已经被放置的皇后
+        if (c == a[i]) or -- 是否同一列
+            (c - n == a[i] - i) or  -- 是否同一对角线
+                (c + n == a[i] + i) then -- 是否同一对角线
+            return false -- 位置会被攻击
+        end
+    end
+    return true -- 位置不会被攻击
+end
+
+-- 打印棋盘
+function printsolution(a)
+    for i = 1, N do -- 对于每一行
+        for j = 1, N do -- 对于每一列
+            -- 输出 "X" 或 “-”， 外加一个空格
+            io.write(a[i] == j and "X" or "-", " ")
+        end
+        io.write("\n")
+    end
+end
+
+-- 把从 'n' 到 'N' 的所有皇后放在棋盘 'a' 上
+function addqueen(a, n)
+    if n > N then -- 是否所有的皇后都被放置好了
+        printsolution(a)
+        os.exit() -- 第一次打印后直接中断程序
+    else -- 尝试放置第n个皇后
+        for c = 1, N do
+            if isplaceok(a, n, c) then
+                a[n] = c -- 把第n个皇后放在列c上
+                addqueen(a, n + 1)
+            end
+        end
+    end
+end
+
+
+-- run
+a = {}
+addqueen(a, 1)
