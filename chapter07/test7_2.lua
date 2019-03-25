390ed15f91d45fb2761da60be7bdb736baba8b15
yoyboy <534411590@qq.com>
2019-03-25 14:15:10 +0800

    update chapter07

--- chapter07/test7_2.lua
+++ chapter07/test7_2.lua
@@ -52,20 +52,18 @@ function sortFileWords(...)
         for line in io.lines() do
             dealStream(line)
         end
-    else if (file and not target) then -- 第二种情况
+    elseif (file and not target) then -- 第二种情况
         pFile = assert(io.open(file, "r"))
         for line in pFile:lines() do
             dealStream(line)
         end
-    else if (file and target) then -- 第三种情况
+    elseif (file and target) then -- 第三种情况
         pFile = assert(io.open(file, "r"))
-        -- tFile = assert(io.open(target, "w"))
         confirmDir(target)
+        tFile = assert(io.open(target, "w"))
         for line in pFile:lines() do
             dealStream(line, target)
         end
-    end
-    end
     end
 end
 
