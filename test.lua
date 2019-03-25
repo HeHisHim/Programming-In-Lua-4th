9d340c8da3a314290742959bc336c0eacdc837cf
yoyboy <534411590@qq.com>
2019-03-25 19:07:17 +0800

    inessential

--- test.lua
+++ test.lua
@@ -1,9 +1,3 @@
-lib = {}
+s = "\1abcdefg"
 
-lib.foo = function (x, y) return x + y end
-lib.goo = function (x, y) return x - y end
-lib.hoo = function (x, y) return x == y end
-
-print(lib.foo(1, 2))
-print(lib.goo(2, 3))
-print(lib.hoo(2, 2))
+print(string.gsub(s, "(.)", "x"))
